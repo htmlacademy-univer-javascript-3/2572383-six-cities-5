6188e9b2218d097d7cc0362db5a7df3a82738926
Nikita Batuev <batueff.nikita@yandex.ru>
2025-01-04 21:29:15 +0500

    module9-task1

--- src/components/sign-out-button/sign-out-button.test.tsx
+++ src/components/sign-out-button/sign-out-button.test.tsx
@@ -3,20 +3,9 @@ import {screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import {SignOutButton} from './sign-out-button.tsx';
-import {Path} from '../../enums/path';
 import {withStore, renderWithRouter} from '../../utils/mock-component.tsx';
 import {ApiRoute} from '../../enums/api-route.ts';
 
-const mockedUseNavigate = vi.fn();
-vi.mock('react-router-dom', async () => {
-  const mod = await vi.importActual<typeof import('react-router-dom')>(
-    'react-router-dom'
-  );
-  return {
-    ...mod,
-    useNavigate: () => mockedUseNavigate,
-  };
-});
 
 describe('Component: SignOutButton', () => {
   beforeEach(() => {
@@ -36,8 +25,6 @@ describe('Component: SignOutButton', () => {
       expect(mockAxiosAdapter.history.delete).toHaveLength(1);
       expect(mockAxiosAdapter.history.delete[0].url).toBe(ApiRoute.Logout);
     });
-
-    expect(mockedUseNavigate).toHaveBeenCalledWith(Path.MainPage);
 
     const actions = mockStore.getActions();
     expect(actions.length).toBeGreaterThanOrEqual(2);
